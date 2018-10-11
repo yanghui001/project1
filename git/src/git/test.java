7cace7ac6a9a434184bb98a98d1c4a3f64f5a609
yanghui001 <44051534+yanghui001@users.noreply.github.com>
2018-10-11 16:09:49 +0800

    Update test.java

--- git/src/git/test.java
+++ git/src/git/test.java
@@ -3,5 +3,6 @@ package git;
 public class test {
     public static void main(String[] args) {
 		System.out.println("yanghui is longmin dog");
+	    System.out.println("yanghui is longmin dogss");
 	}
 }
